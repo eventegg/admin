0c797a1eb4d881d1622e59bb530681131b484a3e
Ömer Başoğlu <obasoglu@eventegg.com>
2013-07-30 11:27:42 +0300

    as

--- EventEgg.Admin/Library/Extensions.cs
+++ EventEgg.Admin/Library/Extensions.cs
@@ -9,5 +9,32 @@ namespace EventEgg.Admin.Library
         {
 
         }
+
+        public static string RadioButtonList<T>(this IViewModelContainer<T> container, Expression<Func<T, object>> expression, IEnumerable<string> items) where T : class
+        {
+            var func = expression.Compile();
+            var result = func(container.ViewModel);
+            var selectList = new SelectList(items, result);
+            return container.RadioButtonList(expression, selectList);
+        }
+
+        public static string RadioButtonList<T>(this IViewModelContainer<T> container, Expression<Func<T, object>> expression, IEnumerable<SelectListItem> items) where T : class
+        {
+            TagBuilder tableTag = new TagBuilder("table");
+            tableTag.AddCssClass("radio-main");
+
+            var trTag = new TagBuilder("tr");
+            foreach (var item in items)
+            {
+                var tdTag = new TagBuilder("td");
+                var radioTag = container.RadioButton(expression).Value(item.Value ?? item.Text).Checked(item.Selected).LabelAfter(item.Text);
+                tdTag.InnerHtml = radioTag.ToString();
+
+                trTag.InnerHtml += tdTag.ToString();
+            }
+            tableTag.InnerHtml = trTag.ToString();
+
+            return tableTag.ToString();
+        }
     }
 }
